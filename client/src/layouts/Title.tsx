d394e3d02acb4547c25271403a34d94f8514ec8b
luosijie <luo_sj@163.com>
2024-09-14 08:32:10 +0800

    update

--- client/src/layouts/Title.tsx
+++ client/src/layouts/Title.tsx
@@ -1,17 +1,24 @@
 import { ArrowLeftCircleIcon, Home } from 'lucide-react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+
+const classIcon = 'text-gray-500 hover:text-black'
 
 const Layout = () => {
+
+    const navigate = useNavigate()
+
+    // navigate(-1)
+
     return (
-        <div className='w-full h-full p-4 flex flex-col'>
+        <div className='w-full h-full p-4 flex flex-col cursor-pointer'>
             <div className='flex justify-between'>
                 {/* <div className="text-xl font-bold">
                     <span className="p-2 text-white bg-black rounded-tl-3xl rounded-br-3xl">Go</span>  chat 
                 </div> */}
-                <Link to="/">
+                <Link to="/" className={classIcon}>
                     <Home size={28}/>
                 </Link>
-                <ArrowLeftCircleIcon size={28}/>
+                <ArrowLeftCircleIcon size={28} className={classIcon} onClick={() => navigate(-1)}/>
             </div>
             <div className='flex-grow'>
                 <Outlet/>
--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,18 +1,23 @@
 import { Lock, User } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
+const classItem = `
+    flex text-gray-500 gap-2 border mt-8 w-full px-4 rounded-md h-16 items-center
+    [&>input]:flex-grow [&>input]:bg-transparent
+`
+
 const Login = () => {
     return (
         <div className="flex flex-col justify-center items-center w-full">
             <div className="text-3xl font-bold"><span className="p-2 text-white bg-black rounded-tl-3xl rounded-br-3xl">Go</span>  chat <span className="mx-5">|</span> Login </div>
             <div className='w-96'>
-                <div className='flex text-gray-500 gap-2 border mt-8 w-full p-4 rounded-md'>
+                <div className={classItem}>
                     <User/>
-                    <input type="text" placeholder='Username' className='flex-grow'/>
+                    <input type="text" placeholder='Username'/>
                 </div>
-                <div className='flex text-gray-500 gap-2 border mt-5 w-full p-4 rounded-md'>
+                <div className={classItem}>
                     <Lock/>
-                    <input type="text" placeholder='Password' className='flex-grow'/>
+                    <input type="text" placeholder='Password'/>
                 </div>
                 <div className='w-full p-4 bg-black rounded-md text-white mt-5 text-center font-bold text-xl'>
                 Submit
